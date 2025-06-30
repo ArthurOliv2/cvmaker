0e6e686756a93ae8fed97ea98ed0b3f3db241595
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-06-30 14:39:35 -0300

    modulariza pagina Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/NavBar"
 import HeroSection from "./components/HeroSection";
 import Benefits from "./components/Benefits";
 import CallToAction from "./components/CallToAction";
-import Footer from "../../components/footer";
+import Footer from "../../components/Footer";
 
 function Home() {
     return (
