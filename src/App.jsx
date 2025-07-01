0c2ea633ea77e60ee26511b8a0b77685a4c8b5dd
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-07-01 15:57:12 -0300

    acrescenta nova funcionalidade 'CRIAR CURRICULO'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Contato from "./pages/Contato";
+import CriarCurriculo from "./pages/CriarCurriculo";
 
 function App() {
     return(
@@ -9,6 +10,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/contato" element={<Contato />} />
+            <Route path="/criar-curriculo" element={<CriarCurriculo />} />
         </Routes>
     )
 }
---
+++ src/pages/CriarCurriculo/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+
+function CriarCurriculo() {
+    return(
+        <>
+            <Navbar />
+            <Footer />
+        </>
+    );
+}
+
+export default CriarCurriculo;
