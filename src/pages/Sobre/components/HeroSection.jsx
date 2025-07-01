02d6ccb4f877998a368c6e9d0bd70702c990203e
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-06-30 22:31:51 -0300

    cria HeroSection dentro da aba SOBRE

--- src/pages/Sobre/components/HeroSection.jsx
+++ src/pages/Sobre/components/HeroSection.jsx
@@ -1,16 +1,11 @@
 function HeroSection() {
     return(
-        <section className="bg-[#0097B2] text-white pt-28 pb-10 px-6 text-center">
-            <h1 className="text-4xl md:text-5xl font-poppins font-bold mb-4">Sobre o Projeto</h1>
-            <p className="text-justify leading-relaxed text-lg md:text-xl max-w-6xl mx-auto font-roboto">
-                O CVMaker nasceu com um objetivo claro: ajudar qualquer pessoa a criar um currículo profissional com rapidez, simplicidade e zero complicação. <br />
-
-                Em um cenário onde a concorrência no mercado de trabalho é cada vez maior, percebemos que muitas ferramentas de criação de currículo são confusas, pagas ou simplesmente inacessíveis para boa parte das pessoas. <br />
-
-                O CVMaker resolve isso oferecendo uma plataforma 100% gratuita, intuitiva e direta, onde o usuário só precisa preencher seus dados para gerar um currículo pronto para enviar — sem cadastros, sem etapas desnecessárias e com um visual limpo e moderno. <br />
-
-                Se você está começando sua carreira, buscando uma nova vaga ou apenas quer renovar seu currículo, aqui é o seu lugar. <br />
-
+        <section className="bg-[#F9FBFC] pt-28 px-6 text-center">
+            <h1 className="text-4xl md:text-5xl font-poppins font-bold text-[#2E2E2E] mb-8">
+                Mais que um site, uma missão
+            </h1>
+            <p className="text-[#6C757D] text-base md:text-lg font-roboto max-w-2xl mx-auto leading-relaxed">
+                Criar um currículo pode parecer simples, mas é o primeiro passo rumo a uma oportunidade. O CVMaker existe para tornar isso possivel para todos com simplicidade, acessibilidade e rapides
             </p>
         </section>
     )
