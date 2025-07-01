b44e3f56f923340b4371089c182394aa9bdcc1b7
ArthurOliv2 <arthuroliveirap2014@gmail.com>
2025-07-01 14:00:53 -0300

    atualizando visual da pagina de contato

--- src/pages/Contato/components/HeroSection.jsx
+++ src/pages/Contato/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 function HeroSection() {
     return (
-        <section className="pt-28 pb-20 px-6 bg-[#F9FBFC] text-center">
+        <section className="pt-28 pb-10 px-6 bg-[#F9FBFC] text-center">
             <h1 className="text-4xl md:text-5xl font-bold text-[#2E2E2E] font-poppins mb-4">
                 Fale com a gente
             </h1>
